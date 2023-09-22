d6708ab97b4172b335fdc5235a661ec547753e97
Kordeliia <lalvmun@hotmail.com>
2023-09-22 10:46:13 +0200

    commit configurar mainactivity.kt con el recycleview

--- List/app/src/main/java/com/cursosandroidant/list/MainActivity.kt
+++ List/app/src/main/java/com/cursosandroidant/list/MainActivity.kt
@@ -2,7 +2,9 @@ package com.cursosandroidant.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cursosandroidant.list.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 /****
  * Project: List
@@ -14,12 +16,35 @@ import com.cursosandroidant.list.databinding.ActivityMainBinding
  * https://www.udemy.com/user/alain-nicolas-tello/
  * Web: www.alainnicolastello.com
  ***/
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+        setupBotton()
+    }
+
+    private fun setupBotton() {
+        binding.btnSave.setOnClickListener {
+            val itemEntity = ItemEntity(Random.nextLong(), binding.etText.text.toString())
+            //Accion de salvar para siguiente apartado
+        }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = ItemAdapter(this)
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.adapter
+        }
+    }
+
+    override fun onClick(itemEntity: Any) {
+        
     }
 }
